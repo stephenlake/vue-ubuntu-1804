da258a10171aa0ec942b374c3fe23f7af7a1d6c2
Stephen Lake <stephen@slvrcld.com>
2020-03-22 18:47:52 +0200

    Add environment split

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,23 @@
-require('laravel-mix')
-    .setPublicPath('./')
-    .disableSuccessNotifications()
-    .autoload({
-        jquery: ['$', 'jQuery', 'window.jQuery'],
-        popper: ['Popper'],
-    })
-    .js('vue/main.js', 'assets/js/app.js')
-    .extract([
-        'axios',
-        'collect.js',
-        'vue',
-        'vue-router',
-        'vuex',
-    ])
+let mix = require('laravel-mix')
+
+mix
+  .setPublicPath('./')
+  .disableSuccessNotifications()
+  .autoload({
+    jquery: ['$', 'jQuery', 'window.jQuery'],
+    popper: ['Popper'],
+  })
+  .js('vue/main.js', 'assets/js/app.js')
+  .extract([
+    'axios',
+    'collect.js',
+    'vue',
+    'vue-router',
+    'vuex',
+  ])
+
+if (mix.inProduction()) {
+  mix.copy('index.prod.html', 'index.html')
+} else {
+  mix.copy('index.dev.html', 'index.html')
+}
